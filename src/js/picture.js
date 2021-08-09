58d772ada49086684c91c1185f3918e2c4ca7f41
christiankriegsheim <39035968+christiankriegsheim@users.noreply.github.com>
2021-08-09 21:22:45 +0200

    update

--- src/js/picture.js
+++ src/js/picture.js
@@ -2,8 +2,8 @@
 
 module.exports = (ImageName, ImageAlt) => {
   return `<picture>
-            <source srcset="/assets/img/${ImageName}?nf_resize=fit&w=700" media="(min-width: 1200px)">
-            <source srcset="/assets/img/${ImageName}?nf_resize=fit&w=600" media="(min-width: 740px)">
-            <img src="/assets/img/${ImageName}?nf_resize=fit&w=500" alt="${ImageAlt}" />
+            <source srcset="/assets/img/${ImageName}?nf_resize=fit&w=1200" media="(min-width: 1200px)">
+            <source srcset="/assets/img/${ImageName}?nf_resize=fit&w=800" media="(min-width: 768px)">
+            <img src="/assets/img/${ImageName}?nf_resize=fit&w=700" alt="${ImageAlt}" />
           </picture>`;
 };
