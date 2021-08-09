a1cec30836d38517396e63193e19b07fba2b4ab5
christiankriegsheim <39035968+christiankriegsheim@users.noreply.github.com>
2021-08-09 20:40:00 +0200

    update

--- src/js/lazy-picture.js
+++ src/js/lazy-picture.js
@@ -1,7 +1,7 @@
 // Generate an picture tag with image src URLs which use Neltify image transforms
 
 module.exports = (ImageName, ImageAlt) => {
-  return `<picture class="lazy lazy_initial">
+  return `<picture class="lazy lazy-initial">
             <source srcset="/assets/img/tiny/${ImageName}" media="(min-width: 1200px)">
             <source srcset="/assets/img/tiny/${ImageName}" media="(min-width: 740px)">
             <img src="/assets/img/tiny/${ImageName}" alt="${ImageAlt}" />
--- src/js/lazy.js
+++ src/js/lazy.js
@@ -16,14 +16,14 @@ function loadImage(picture) {
   for(var s=0; s<sources.length; s++) {
     // update the src or srcset urls
     if (sources[s].hasAttribute("srcset")) {
-      updateAttributeURL(sources[s], "srcset", loadingPath, "assets/img/"+sizes[s] );
+      updateAttributeURL(sources[s], "srcset", loadingPath, "img/"+sizes[s] );
     } else {
-      updateAttributeURL(sources[s], "src", loadingPath, "assets/img/"+sizes[s] );
+      updateAttributeURL(sources[s], "src", loadingPath, "img/"+sizes[s] );
     }
 
     // remove the lazy-initial class when the full image is loaded to unblur
     sources[s].addEventListener('load', image => {
-      image.target.closest("picture").classList.remove("lazy_initial")
+      image.target.closest("picture").classList.remove("lazy-initial")
     }, false);
   }
 
