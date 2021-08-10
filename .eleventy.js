0103717b8288698946658d9d977c2f5705c6859c
christiankriegsheim <39035968+christiankriegsheim@users.noreply.github.com>
2021-08-10 21:49:16 +0200

    update

--- .eleventy.js
+++ .eleventy.js
@@ -7,11 +7,11 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/style.css");
 
   // A responsive image helper using Netlify Large Media - image transformation
-  eleventyConfig.addShortcode("picture_w", require("./src/js/picture_l.js"));
-  eleventyConfig.addShortcode("picture_h", require("./src/js/picture_p.js"));
+  eleventyConfig.addShortcode("picture_l", require("./src/js/picture_l.js"));
+  eleventyConfig.addShortcode("picture_p", require("./src/js/picture_p.js"));
   // A lazy loading image helper using Netlify Large Media - image transformation
-  eleventyConfig.addShortcode("lazypicture_w", require("./src/js/lazy-picture_l.js"));
-  eleventyConfig.addShortcode("lazypicture_h", require("./src/js/lazy-picture_p.js"));
+  eleventyConfig.addShortcode("lazypicture_l", require("./src/js/lazy-picture_l.js"));
+  eleventyConfig.addShortcode("lazypicture_p", require("./src/js/lazy-picture_p.js"));
 
   // date filter (localized)
   eleventyConfig.addNunjucksFilter("date", function (date, format, locale) {
