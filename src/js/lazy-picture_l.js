89733eaf6c971edf8edbf831dd468f0e15f2d3f0
christiankriegsheim <39035968+christiankriegsheim@users.noreply.github.com>
2021-08-10 21:41:20 +0200

    Update

--- src/js/lazy-picture_l.js
+++ src/js/lazy-picture_l.js
@@ -2,9 +2,13 @@
 
 module.exports = (ImageName, ImageAlt) => {
   return `<picture class="lazy lazy-initial">
-            <source srcset="/assets/img/${ImageName}" media="(min-width: 1025px)">
-            <source srcset="/assets/img/${ImageName}?nf_resize=fit&w=1024" media="(min-width: 769px)">
-            <source srcset="/assets/img/${ImageName}?nf_resize=fit&w=768" media="(min-width: 481px)">
-            <img src="/assets/img/${ImageName}?nf_resize=fit&w=480" alt="${ImageAlt}" />
+            <source srcset="/assets/img/${ImageName}.webp" type="image/webp" media="(min-width: 1025px)">
+            <source srcset="/assets/img/${ImageName}.webp?nf_resize=fit&w=1024" type="image/webp" media="(min-width: 769px)">
+            <source srcset="/assets/img/${ImageName}.webp?nf_resize=fit&w=768" type="image/webp" media="(min-width: 481px)">
+
+            <source srcset="/assets/img/${ImageName}.jpg" type="image/jpeg" media="(min-width: 1025px)">
+            <source srcset="/assets/img/${ImageName}.jpg?nf_resize=fit&w=1024" type="image/jpeg" media="(min-width: 769px)">
+            <source srcset="/assets/img/${ImageName}.jpg?nf_resize=fit&w=768" type="image/jpeg" media="(min-width: 481px)">
+            <img src="/assets/img/${ImageName}.jpg?nf_resize=fit&w=480" alt="${ImageAlt}" />
           </picture>`;
 };
