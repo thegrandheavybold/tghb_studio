b98f4e6d63e79a3579dc78bec894b2a42f772085
christiankriegsheim <39035968+christiankriegsheim@users.noreply.github.com>
2021-08-11 21:31:03 +0200

    update

--- src/js/lazy-picture_l.js
+++ src/js/lazy-picture_l.js
@@ -2,12 +2,12 @@
 
 module.exports = (ImageName, ImageAlt) => {
   return `<picture class="lazy lazy-initial">
+            <source srcset="/assets/img/${ImageName}.webp" type="image/webp">
 
+            <source srcset="/assets/img/tiny/${ImageName}.jpg?nf_resize=fit&w=1024" type="image/jpeg" media="(max-width: 1024px)">
+            <source srcset="/assets/img/tiny/${ImageName}.jpg?nf_resize=fit&w=768" type="image/jpeg" media="(max-width: 768px)">
+            <source srcset="/assets/img/tiny/${ImageName}.jpg?nf_resize=fit&w=480" type="image/jpeg" media="(max-width: 480px)">
 
-            <source srcset="/assets/img/tiny/${ImageName}.jpg" type="image/jpeg" media="(min-width: 1025px)">
-            <source srcset="/assets/img/tiny/${ImageName}.jpg?nf_resize=fit&w=1024" type="image/jpeg" media="(min-width: 769px)">
-            <source srcset="/assets/img/tiny/${ImageName}.jpg?nf_resize=fit&w=768" type="image/jpeg" media="(min-width: 481px)">
-
-            <img src="/assets/img/tiny/${ImageName}.jpg?nf_resize=fit&w=480" alt="${ImageAlt}" />
+            <img src="/assets/img/tiny/${ImageName}.jpg" alt="${ImageAlt}" />
           </picture>`;
 };
--- src/js/picture_p.js
+++ src/js/picture_p.js
@@ -4,10 +4,11 @@ module.exports = (ImageName, ImageAlt) => {
   return `<picture>
             <source srcset="/assets/img/${ImageName}.webp" type="image/webp">
 
-            <source srcset="/assets/img/${ImageName}.jpg" type="image/jpeg" media="(min-width: 1025px)">
-            <source srcset="/assets/img/${ImageName}.jpg?nf_resize=fit&w=1024" type="image/jpeg" media="(min-width: 769px)">
-            <source srcset="/assets/img/${ImageName}.jpg?nf_resize=fit&w=768" type="image/jpeg" media="(min-width: 481px)">
+            <source srcset="/assets/img/${ImageName}.jpg?nf_resize=fit&w=480" type="image/jpeg" media="(max-width: 1024px)">
+            <source srcset="/assets/img/${ImageName}.jpg?nf_resize=fit&w=768" type="image/jpeg" media="(max-width: 768px)">
+            <source srcset="/assets/img/${ImageName}.jpg?nf_resize=fit&w=480" type="image/jpeg" media="(max-width: 480px)">
 
-            <img src="/assets/img/${ImageName}.jpg?nf_resize=fit&w=480" alt="${ImageAlt}" />
+            <img src="/assets/img/${ImageName}.jpg" alt="${ImageAlt}" />
+
           </picture>`;
 };
