bb3567e60854b753dd0f5cf476ee38661555d12c
christiankriegsheim <39035968+christiankriegsheim@users.noreply.github.com>
2021-08-11 21:40:39 +0200

    update

--- src/js/lazy-picture_p.js
+++ src/js/lazy-picture_p.js
@@ -2,7 +2,7 @@
 
 module.exports = (ImageName, ImageAlt) => {
   return `<picture class="lazy lazy-initial">
-            <source srcset="/assets/img/${ImageName}.webp" type="image/webp">
+            
 
             <source srcset="/assets/img/tiny/${ImageName}.jpg?nf_resize=fit&w=480" type="image/jpeg" media="(max-width: 1024px)">
             <source srcset="/assets/img/tiny/${ImageName}.jpg?nf_resize=fit&w=768" type="image/jpeg" media="(max-width: 768px)">
