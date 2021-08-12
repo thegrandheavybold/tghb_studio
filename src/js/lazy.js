4c616c915078d663af232eb850a9e1a89bd47c09
christiankriegsheim <39035968+christiankriegsheim@users.noreply.github.com>
2021-08-12 20:39:45 +0200

    Update

--- src/js/lazy.js
+++ src/js/lazy.js
@@ -9,8 +9,7 @@ function updateAttributeURL(element, attr, swapOut, swapIn) {
 // Update the image source on elements in the picture element
 function loadImage(picture) {
 
-  var sources = picture.children;
-  var sources = figure.children;
+  var sources = ["picture.children", "figure.children"];
   var loadingPath = "assets/img/tiny";
   var sizes = ["original","large","medium","small"];
 
