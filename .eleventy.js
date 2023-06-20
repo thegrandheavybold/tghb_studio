f9a00933f6fdd8a12f8622742e6be7dadb9bb254
kriegsheim <mail@kriegsheim.com>
2023-06-20 11:21:31 +0200

    Update

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
 
   const moment = require("moment");
 
+  eleventyConfig.addPassthroughCopy("./src/js/main.js");
   eleventyConfig.addPassthroughCopy("./src/js/main-min.js");
   eleventyConfig.addPassthroughCopy("./src/js/main-min.js.map");
   eleventyConfig.addPassthroughCopy("./src/assets");
